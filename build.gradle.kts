a658243c9287322ae185178918365ba9996bd2c4
Jinho Yim <jinhoyim@outlook.com>
2024-10-04 19:50:45 +0900

    스프링 부트 테스트 환경을 위해서 루트 모듈의 spring-boot-starter, spring-boot-starter-test 의존성 패키지 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ subprojects {
 
         implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-//        implementation("org.springframework.boot:spring-boot-starter")
-//        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        implementation("org.springframework.boot:spring-boot-starter")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
