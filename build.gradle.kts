91cf4abc365ac66e35bf609465164f138ac3c0ff
Jinho Yim <jinhoyim@outlook.com>
2024-10-08 21:08:25 +0900

    스프링 부트 플러그인을 루트 모듈의 빌드 구성으로 이동

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.25"
 //    kotlin("plugin.spring") version "1.9.25"
     kotlin("kapt") version "1.9.25"
-//    id("org.springframework.boot") version "3.3.4"
+    id("org.springframework.boot") version "3.3.4"
     id("org.jetbrains.kotlin.plugin.spring") version "1.9.25"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -28,6 +28,7 @@ subprojects {
         plugin("kotlin-kapt")
         plugin("io.spring.dependency-management")
         plugin("kotlin-spring")
+        plugin("org.springframework.boot")
     }
 
     dependencies {
